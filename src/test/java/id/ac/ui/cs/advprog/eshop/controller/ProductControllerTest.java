25ab5bb6cc8dec61fc4a0768ab8241c6a10c8835
daffuwu <dananda41661@gmail.com>
2026-02-18 01:04:05 +0700

    Add unit tests for product controller

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -0,0 +1,89 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ProductController.class)
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService service;
+
+    @Test
+    void testCreateProductPage() throws Exception {
+        mockMvc.perform(get("/product/create"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("createProduct"))
+                .andExpect(model().attributeExists("product"));
+    }
+
+    @Test
+    void testCreateProductPost() throws Exception {
+        mockMvc.perform(post("/product/create")
+                        .param("productName", "Sampo Cap Bambang")
+                        .param("productQuantity", "100"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("list"));
+
+        verify(service, times(1)).create(any(Product.class));
+    }
+
+    @Test
+    void testEditProductPage() throws Exception {
+        Product product = new Product();
+        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        when(service.findById("eb558e9f-1c39-460e-8860-71af6af63bd6")).thenReturn(product);
+
+        mockMvc.perform(get("/product/edit/eb558e9f-1c39-460e-8860-71af6af63bd6"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("editProduct"))
+                .andExpect(model().attribute("product", product));
+    }
+
+    @Test
+    void testEditProductPost() throws Exception {
+        mockMvc.perform(post("/product/edit")
+                        .param("productId", "eb558e9f-1c39-460e-8860-71af6af63bd6")
+                        .param("productName", "Sampo Cap Usep")
+                        .param("productQuantity", "50"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("list"));
+
+        verify(service, times(1)).update(any(Product.class));
+    }
+
+    @Test
+    void testDeleteProduct() throws Exception {
+        mockMvc.perform(get("/product/delete/eb558e9f-1c39-460e-8860-71af6af63bd6"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("../list"));
+
+        verify(service, times(1)).delete("eb558e9f-1c39-460e-8860-71af6af63bd6");
+    }
+
+    @Test
+    void testProductListPage() throws Exception {
+        List<Product> allProducts = Arrays.asList(new Product(), new Product());
+        when(service.findAll()).thenReturn(allProducts);
+
+        mockMvc.perform(get("/product/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("productList"))
+                .andExpect(model().attribute("products", allProducts));
+    }
+}
