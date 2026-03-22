9e596ed3fefa04326079437e11245d4a4cf854ae
daffuwu <dananda41661@gmail.com>
2026-03-23 00:42:53 +0700

    [RED] Add functional tests for OrderController

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/OrderControllerTest.java
@@ -0,0 +1,53 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.service.OrderService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(OrderController.class)
+class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService orderService;
+
+    @Test
+    void testGetCreateOrderPage() throws Exception {
+        mockMvc.perform(get("/order/create"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("createOrder"));
+    }
+
+    @Test
+    void testGetOrderHistoryPage() throws Exception {
+        mockMvc.perform(get("/order/history"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("orderHistoryForm"));
+    }
+
+    @Test
+    void testPostOrderHistory() throws Exception {
+        when(orderService.findAllByAuthor(anyString())).thenReturn(new ArrayList<>());
+
+        mockMvc.perform(post("/order/history")
+                        .param("author", "Safira"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("orderHistoryList"))
+                .andExpect(model().attributeExists("orders"))
+                .andExpect(model().attribute("author", "Safira"));
+    }
+}
